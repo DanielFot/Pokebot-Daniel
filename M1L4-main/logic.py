631e8c3b4c4d54e1f163ad4f63497a396fd154e3
DanielFot <danifot22@gmail.com>
2026-03-12 17:36:00 +0100

    Improved Pokebot

--- M1L4-main/logic.py
+++ M1L4-main/logic.py
@@ -1,84 +1,115 @@
 from random import randint
 import requests
 
-
 class Pokemon:
     pokemons = {}
 
-
     def __init__(self, pokemon_trainer):
-
         self.pokemon_trainer = pokemon_trainer
+
         self.pokemon_number = randint(1, 1000)
 
-        data = self.get_data()
+        self.name = self.get_name()
+        self.img = self.get_img()
 
-        self.name = data["name"]
-        self.img = data["img"]
-        self.type = data["type"]
-        self.height = data["height"]
-        self.weight = data["weight"]
-        self.hp = data["hp"]
-        self.attack = data["attack"]
-        self.defense = data["defense"]
+
+        self.hp = randint(80, 120)
+        self.power = randint(10, 20)
 
         Pokemon.pokemons[pokemon_trainer] = self
 
+    def heal(self):
+        self.hp += 20
 
-    def get_data(self):
-        url = f'https://pokeapi.co/api/v2/pokemon/{self.pokemon_number}'
+    def get_name(self):
+        url = f"https://pokeapi.co/api/v2/pokemon/{self.pokemon_number}"
         response = requests.get(url)
 
         if response.status_code == 200:
-            data = response.json()
+            return response.json()["name"]
 
-            pokemon_data = {
-                "name": data["name"],
-                "img": data["sprites"]["other"]["dream_world"]["front_default"],
-                "type": data["types"][0]["type"]["name"],
-                "height": data["height"],
-                "weight": data["weight"],
-                "hp": data["stats"][0]["base_stat"],
-                "attack": data["stats"][1]["base_stat"],
-                "defense": data["stats"][2]["base_stat"]
-            }
+        return "pikachu"
 
-            return pokemon_data
+    def get_img(self):
+        url = f"https://pokeapi.co/api/v2/pokemon/{self.pokemon_number}"
+        response = requests.get(url)
 
-        return {
-            "name": "pikachu",
-            "img": "https://static.wikia.nocookie.net/pokemon/images/0/0d/025Pikachu.png",
-            "type": "electric",
-            "height": 4,
-            "weight": 60,
-            "hp": 100,
-            "attack": 50,
-            "defense": 40
-        }
+        if response.status_code == 200:
+            return response.json()['sprites']['other']['dream_world']['front_default']
 
+        return None
 
     def info(self):
         return (
-            f"Your Pokémon: {self.name}\n"
-            f"Type: {self.type}\n"
-            f"Height: {self.height}\n"
-            f"Weight: {self.weight}\n"
+            f"Pokemon name: {self.name}\n"
             f"HP: {self.hp}\n"
-            f"Attack: {self.attack}\n"
-            f"Defense: {self.defense}"
+            f"Power: {self.power}"
         )
-
 
     def show_img(self):
         return self.img
 
-    def take_damage(self, damage):
-        self.hp -= damage
-        if self.hp < 0:
-            self.hp = 0
-
-    # heal pokemon
-    def heal(self, amount):
-        self.hp += amount
+    # attack method
+    def attack(self, enemy):
 
 
+        if isinstance(enemy, Wizard):
+            chance = randint(1, 5)
+            if chance == 1:
+                return "The Wizard Pokémon used a magical shield!"
+
+        if enemy.hp > self.power:
+            enemy.hp -= self.power
+            return f"@{self.pokemon_trainer} battles @{enemy.pokemon_trainer}"
+        else:
+            enemy.hp = 0
+
+
+            self.power += 2
+            self.hp += 5
+
+            return f"@{self.pokemon_trainer} defeats @{enemy.pokemon_trainer}!"
+
+class Wizard(Pokemon):
+
+    def __init__(self, pokemon_trainer):
+        super().__init__(pokemon_trainer)
+
+
+        self.hp += 30
+
+    def info(self):
+        parent_info = super().info()
+        return "You have a Wizard Pokémon\n" + parent_info
+
+    def attack(self, enemy):
+        return super().attack(enemy)
+
+class Fighter(Pokemon):
+
+    def __init__(self, pokemon_trainer):
+        super().__init__(pokemon_trainer)
+
+
+        self.power += 10
+
+    def info(self):
+        parent_info = super().info()
+        return "You have a Fighter Pokémon\n" + parent_info
+
+    def attack(self, enemy):
+        super_strength = randint(5, 15)
+        self.power += super_strength
+        result = super().attack(enemy)
+        self.power -= super_strength
+        return result + f"\nThe fighter used a super attack with strength: {super_strength}"
+
+if __name__ == '__main__':
+    wizard = Wizard("username1")
+    fighter = Fighter("username2")
+
+    print(wizard.info())
+    print()
+    print(fighter.info())
+    print()
+    print(fighter.attack(wizard))
--- M1L4-main/main.py
+++ M1L4-main/main.py
@@ -1,21 +1,53 @@
 import telebot
 from config import token
-from logic import Pokemon
+from logic import Pokemon, Wizard, Fighter
+from random import randint
 
 bot = telebot.TeleBot(token)
-
+bot.infinity_polling(none_stop=True)
 
 @bot.message_handler(commands=['go'])
 def go(message):
-    username = message.from_user.username
 
-    if username not in Pokemon.pokemons:
-        pokemon = Pokemon(username)
+    if message.from_user.username not in Pokemon.pokemons.keys():
+
+        chance = randint(1, 10)
+
+        if chance == 1:
+            pokemon = Wizard(message.from_user.username)
+        elif chance == 2:
+            pokemon = Fighter(message.from_user.username)
+        else:
+            pokemon = Pokemon(message.from_user.username)
+
         bot.send_message(message.chat.id, pokemon.info())
         bot.send_photo(message.chat.id, pokemon.show_img())
-    else:
-        bot.reply_to(message, "You already have a Pokémon!")
 
+    else:
+        bot.reply_to(message, "Ты уже создал себе покемона")
+
+@bot.message_handler(commands=['battle'])
+def battle(message):
+
+    users = message.text.split()
+
+    if len(users) != 2:
+        bot.send_message(message.chat.id, "Use: /battle @username")
+        return
+
+    attacker = message.from_user.username
+    defender = users[1].replace("@", "")
+
+    if attacker not in Pokemon.pokemons or defender not in Pokemon.pokemons:
+        bot.send_message(message.chat.id, "Both trainers must have Pokémon!")
+        return
+
+    pokemon1 = Pokemon.pokemons[attacker]
+    pokemon2 = Pokemon.pokemons[defender]
+
+    result = pokemon1.attack(pokemon2)
+
+    bot.send_message(message.chat.id, result)
 
 @bot.message_handler(commands=['info'])
 def info(message):
@@ -30,26 +62,75 @@ def info(message):
 
 @bot.message_handler(commands=['attack'])
 def attack(message):
-    username = message.from_user.username
 
-    if username in Pokemon.pokemons:
-        pokemon = Pokemon.pokemons[username]
-        pokemon.take_damage(10)
-        bot.send_message(message.chat.id, f"{pokemon.name} took damage! HP: {pokemon.hp}")
+    if message.reply_to_message:
+
+        attacker = message.from_user.username
+        defender = message.reply_to_message.from_user.username
+
+        if attacker not in Pokemon.pokemons:
+            bot.send_message(message.chat.id, "You don't have a Pokémon. Use /go")
+            return
+
+        if defender not in Pokemon.pokemons:
+            bot.send_message(message.chat.id, "The opponent has no Pokémon.")
+            return
+
+        pokemon1 = Pokemon.pokemons[attacker]
+        pokemon2 = Pokemon.pokemons[defender]
+
+        result = pokemon1.attack(pokemon2)
+
+        bot.send_message(message.chat.id, result)
+
     else:
-        bot.send_message(message.chat.id, "Create a Pokémon first using /go")
+        bot.send_message(
+            message.chat.id,
+            "Reply to a user's message with /attack to start a battle."
+        )
 
 
 @bot.message_handler(commands=['heal'])
-def heal(message):
+def heal_pokemon(message):
+
+    username = message.from_user.username
+
+    if username not in Pokemon.pokemons:
+        bot.send_message(message.chat.id, "Create a Pokémon first using /go")
+        return
+
+    pokemon = Pokemon.pokemons[username]
+
+    pokemon.heal()
+
+    bot.send_message(
+        message.chat.id,
+        f"{pokemon.name} restored health!\nHP: {pokemon.hp}"
+    )
+
+@bot.message_handler(commands=['feed'])
+def feed_pokemon(message):
     username = message.from_user.username
 
     if username in Pokemon.pokemons:
         pokemon = Pokemon.pokemons[username]
-        pokemon.heal(10)
-        bot.send_message(message.chat.id, f"{pokemon.name} healed! HP: {pokemon.hp}")
+        pokemon.feed()
+
+        bot.send_message(
+            message.chat.id,
+            f"You fed {pokemon.name} 🍖\nLevel: {pokemon.level}\nEXP: {pokemon.exp}"
+        )
     else:
         bot.send_message(message.chat.id, "Create a Pokémon first using /go")
 
+@bot.message_handler(commands=['achievements'])
+def achievements(message):
+    username = message.from_user.username
+
+    if username in Pokemon.pokemons:
+        pokemon = Pokemon.pokemons[username]
+        bot.send_message(message.chat.id, pokemon.show_achievements())
+    else:
+        bot.send_message(message.chat.id, "Create a Pokémon first using /go")
 
 bot.infinity_polling(none_stop=True)
